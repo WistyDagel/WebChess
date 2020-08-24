8a47795f910f8d0adb685baa9f8b63281416707a
Jeffrey Rehm <47900191+j-rehm@users.noreply.github.com>
2020-08-24 11:58:33 -0600

    Added piece possible movement validation
    also fixed some svg bugs and removed the massive pngs

--- src/scripts/chess.js
+++ src/scripts/chess.js
@@ -7,6 +7,8 @@ let board = document.getElementById('chessBoard');
 GameBoard.populateBoard(board);
 Pieces.placePieces(board);
 
+const chess = new Chess(Pieces.getStartingState());
+
 board.onclick = (evt) => {
     if (evt.target.id) {
         // A square that does not have a piece on it.
@@ -25,9 +27,9 @@ board.onclick = (evt) => {
     let regexGroupings = regexPattern.exec(focusSquare.childNodes[0].src);
 
     if (focusSquare) {
+        possiblePlaces = manipulate(focusSquare.id, getManipulators(regexGroupings[1], regexGroupings[2]));
         switch(regexGroupings[1]) {
             case "P":
-                possiblePlaces = possiblePawnMoves(focusSquare.id, regexGroupings[2]);
                 break;
             case "R":
                 break;
@@ -50,16 +52,225 @@ board.onclick = (evt) => {
     }
 }
 
-const possiblePawnMoves = (squareId, pieceColor) => {
-    let possibleMovesArray = [];
+// Gets possibls ending locations based on piece color
+// Index 0 is added to the piece column, Index 1 to the row
+const getManipulators = (PieceType, pieceColor) => {
+    let manipulators = [];
 
-    if (squareId.split('')[1] == 7 && pieceColor == 'W') {
-        
+    // TODO Does this work properly?
+    switch(PieceType) {
+        case "P":
+            // TODO debug black pawn movement after turn 1
+            if (pieceColor == 'W') {
+                manipulators = [
+                    [0, 1],
+                    [0, 2],
+                    [-1, 1],
+                    [1, 1]
+                ];
+            } else if (pieceColor == 'B') {
+                manipulators = [
+                    [0, -1],
+                    [0, -2],
+                    [-1, -1],
+                    [1, -1]
+                ];
+            }
+            break;
+        case "R":
+            manipulators = [
+                [0, 1],
+                [0, 2],
+                [0, 3],
+                [0, 4],
+                [0, 5],
+                [0, 6],
+                [0, 7],
+
+                [1, 0],
+                [2, 0],
+                [3, 0],
+                [4, 0],
+                [5, 0],
+                [6, 0],
+                [7, 0],
+
+                [0, -1],
+                [0, -2],
+                [0, -3],
+                [0, -4],
+                [0, -5],
+                [0, -6],
+                [0, -7],
+
+                [-1, 0],
+                [-2, 0],
+                [-3, 0],
+                [-4, 0],
+                [-5, 0],
+                [-6, 0],
+                [-7, 0],
+            ];
+            break;
+        case "N":
+            manipulators = [
+                [-1, 2],
+                [1, 2],
+                [-2, 1],
+                [2, 1],
+                [-1, -2],
+                [1, -2],
+                [-2, -1],
+                [2, -1],
+            ];
+            break;
+        case "B":
+            manipulators = [
+                [1, 1],
+                [2, 2],
+                [3, 3],
+                [4, 4],
+                [5, 5],
+                [6, 6],
+                [7, 7],
+
+                [1, -1],
+                [2, -2],
+                [3, -3],
+                [4, -4],
+                [5, -5],
+                [6, -6],
+                [7, -7],
+
+                [-1, 1],
+                [-2, 2],
+                [-3, 3],
+                [-4, 4],
+                [-5, 5],
+                [-6, 6],
+                [-7, 7],
+
+                [-1, -1],
+                [-2, -2],
+                [-3, -3],
+                [-4, -4],
+                [-5, -5],
+                [-6, -6],
+                [-7, -7]
+            ];
+            break;
+        case "K":
+            manipulators = [
+                [-1, -1],
+                [0, -1],
+                [1, -1],
+
+                [-2, 0], // Castling
+                [-1, 0],
+                [1, 0],
+                [2, 0], // Castling
+
+                [-1, 1],
+                [0, 1],
+                [1, 1],
+            ];
+            break;
+        case "Q":
+            manipulators = [
+                [1, 1],
+                [2, 2],
+                [3, 3],
+                [4, 4],
+                [5, 5],
+                [6, 6],
+                [7, 7],
+
+                [1, -1],
+                [2, -2],
+                [3, -3],
+                [4, -4],
+                [5, -5],
+                [6, -6],
+                [7, -7],
+
+                [-1, 1],
+                [-2, 2],
+                [-3, 3],
+                [-4, 4],
+                [-5, 5],
+                [-6, 6],
+                [-7, 7],
+
+                [-1, -1],
+                [-2, -2],
+                [-3, -3],
+                [-4, -4],
+                [-5, -5],
+                [-6, -6],
+                [-7, -7],
+                
+                [0, 1],
+                [0, 2],
+                [0, 3],
+                [0, 4],
+                [0, 5],
+                [0, 6],
+                [0, 7],
+
+                [1, 0],
+                [2, 0],
+                [3, 0],
+                [4, 0],
+                [5, 0],
+                [6, 0],
+                [7, 0],
+
+                [0, -1],
+                [0, -2],
+                [0, -3],
+                [0, -4],
+                [0, -5],
+                [0, -6],
+                [0, -7],
+
+                [-1, 0],
+                [-2, 0],
+                [-3, 0],
+                [-4, 0],
+                [-5, 0],
+                [-6, 0],
+                [-7, 0],
+            ];
+            break;
+        default:
+            break;
     }
+
+    return manipulators;
+}
+
+const manipulate = (squareId, manipulators) => {
+    let possibleMovesArray = [];
+    let col = squareId.charCodeAt(0); // D 68
+    let row = parseInt(squareId.split('')[1]);
+
+    // Iterate over all the coordinate manipulators
+    manipulators.forEach(m => {
+        // Manipulate the existing coordinate to get possible ending value
+        let coordinate = `${String.fromCharCode(col + m[0])}${row + m[1]}`;
+        // console.log(squareId.toLowerCase());
+        // console.log(coordinate.toLowerCase());
+        // If valid move
+        if (chess.move({ from: squareId.toLowerCase(), to: coordinate.toLowerCase()})) {
+            // Undo move
+            chess.undo();
+            // Add to possible moves array
+            possibleMovesArray.push(coordinate);
+        }
+    });
 
     return possibleMovesArray;
 }
 
-const chess = new Chess(Pieces.getStartingState());
 
 // console.log(chess.board());

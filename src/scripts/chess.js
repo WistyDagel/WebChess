40e7c36a34771be6966e15ead8eaccb646db16b2
DeryGermann <termaiter@gmail.com>
2020-08-24 10:53:34 -0600

    some logic

--- src/scripts/chess.js
+++ src/scripts/chess.js
@@ -8,9 +8,58 @@ GameBoard.populateBoard(board);
 Pieces.placePieces(board);
 
 board.onclick = (evt) => {
+    if (evt.target.id) {
+        // A square that does not have a piece on it.
+    } else {
+        // A square that contains a piece.
+    }
+
+    // Gets the piece that was clicked on.
+    // Otherwise gets null if no piece was clicked.
     var focusSquare = GameBoard.getFocusSquare();
+
+    let regexPattern = new RegExp('..\/images\/pieces\/([A-Z])(B|W).svg');
+
+    let possiblePlaces = [];
+
+    let regexGroupings = regexPattern.exec(focusSquare.childNodes[0].src);
+
+    if (focusSquare) {
+        switch(regexGroupings[1]) {
+            case "P":
+                possiblePlaces = possiblePawnMoves(focusSquare.id, regexGroupings[2]);
+                break;
+            case "R":
+                break;
+            case "N":
+                break;
+            case "B":
+                break;
+            case "K":
+                break;
+            case "Q":
+                break;
+            default:
+                console.log('Cannot find piece.');
+                break;
+        }
+    }
+
+    if (possiblePlaces.length != 0) {
+        console.log(possiblePlaces);
+    }
 }
 
-const chess = new Chess (Pieces.getStartingState());
+const possiblePawnMoves = (squareId, pieceColor) => {
+    let possibleMovesArray = [];
 
-console.log(chess.board());
+    if (squareId.split('')[1] == 7 && pieceColor == 'W') {
+        
+    }
+
+    return possibleMovesArray;
+}
+
+const chess = new Chess(Pieces.getStartingState());
+
+// console.log(chess.board());
